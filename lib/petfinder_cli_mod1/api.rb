6f3e165a845651ed84393750d0d4a7c54d1c4561
M. Gage Morgan <gage@gages.blog>
2020-07-08 01:22:10 -0400

    Couldn't change the name of the project, but got a proper newsfeed api to work

--- lib/petfinder_cli_mod1/api.rb
+++ lib/petfinder_cli_mod1/api.rb
@@ -1,37 +1,34 @@
 require 'json'
-
+require 'open-uri'
 module PetfinderCliMod1
 
   class API
-    API_HOST = "https://api.petfinder.com/v2/"
+    API_HOST = "https://newsapi.org/v2/"
+    TOP_HEADLINES = "top-headlines?"
 
     def self.api_public_key 
       begin
-        @@public = File.open(File.expand_path("~/.petfinder_public")).read.strip
+        @@public = File.open(File.expand_path("~/.newsfeed_key")).read.strip
       rescue 
-        puts "No public key found. Please follow the instructions at https://www.petfinder.com/developers/ to get your keys."
+        puts "No public key found. Please follow the instructions at https://newsapi.org to get your keys."
 
         # create public key
         puts "Please enter your public key:"
         @@public = gets.strip 
         return if @@public == "exit"
-        File.open(File.expand_path("~/.petfinder_public"), "w") do |file|
+        File.open(File.expand_path("~/.newsfeed_key"), "w") do |file|
           file.print @@public
         end
       end
       @@public
     end
 
-    def self.auth_token
-      url = "#{API_HOST}/oauth2/token"
-      query = {
-        grant_type: 'client_credentials',
-        client_id: @@public,
-        client_secret: @@private
-      }
-
-
-      end
+    def self.news_by_country(country)
+      url = "http://newsapi.org/v2/top-headlines?country=#{country}&apiKey=#{@@public}"
+      req = open(url)
+      response_body = req.read
+      puts response_body
+    end
   end
 
 end
--- petfinder_cli_mod1.gemspec
+++ petfinder_cli_mod1.gemspec
@@ -32,6 +32,4 @@ Gem::Specification.new do |spec|
   spec.add_dependency "dotenv"
   spec.add_dependency "httparty"
   spec.add_dependency "http"
-  spec.add_dependency "oauth2"
-  spec.add_dependency "oauth2-client"
 end
